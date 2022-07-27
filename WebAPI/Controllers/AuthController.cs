fd03e9f268c328aa29214dc65c4fcad48ac2322c
Omer Faruk Sandikci <sandikci_omer@hotmail.com>
2022-07-27 14:33:39 +0300

    Add project files.

---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Security;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            var tokenGenerator = new JwtGenerator();
+            var token=tokenGenerator.GenerateToken();
+            return Ok(token);
+        }
+    }
+}
---
+++ WebAPI/Security/JwtGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace WebAPI.Security
+{
+    public class JwtGenerator
+    {
+        public string GenerateToken()
+        {
+            var securityToken = new JwtSecurityToken
+                (
+                    issuer:"deneme",
+                    audience: "deneme",
+                    expires: System.DateTime.UtcNow.AddDays(1),
+                    notBefore: System.DateTime.Now,
+                    signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("security key anahtarı 40 karakterden uzun olmalıdır")),
+                    SecurityAlgorithms.HmacSha256)
+
+                );
+
+            string token=new JwtSecurityTokenHandler().WriteToken(securityToken);
+            return token;
+        }
+    }
+}
